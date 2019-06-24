4887435e0e080b2ce14c069e831ce6f03ebe27e9
M. Elbassiouni <34175926+msm1500@users.noreply.github.com>
2019-06-24 05:43:29 +0200

    my first commit

---
+++ OpenWeather/Default.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="OpenWeather.Default" %>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        body
+        {
+            font-family:Arial;
+            font-size: 14px;
+
+        }
+
+        table th
+        {
+            background-color:#ffffff;
+            color:#333;
+            font-weight: bold;
+        }
+
+        table th, table td
+        {
+            padding: 5px;
+            border: 1px solid #ccc;
+
+        }
+
+           table, table table td
+        {
+            
+            border: 0px solid #ccc;
+
+        }
+
+    </style>
+
+
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        Please enter city name: 
+
+        <asp:TextBox ID="txtCity" runat="server" Text=""/>
+        <asp:Button Text="Check Weather" runat="server" OnClick="GetWeatherInfo" />
+        <hr/>
+        <table id="tblWeather" runat="server" border="0" cellpadding="0" cellspacing="0" visible="false">
+       
+            <th colspan="2">
+               -- Weather info --
+            </th>
+        </tr>
+
+        <tr>
+            <td>
+                City: 
+                <asp:Label ID="lblCity_Country" runat="server"/>.
+
+                <asp:Label ID="lblDescription" runat="server"/>, 
+                Humidity: 
+                <asp:Label ID="lblHumidity" runat="server"/>
+            
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Temp: 
+                Min:
+                <asp:Label ID="lblTempMin" runat="server"/>
+                Max:
+                <asp:Label ID="lblTempMax" runat="server"/>
+                Day:
+                <asp:Label ID="lblTempDay" runat="server"/>
+                Night:
+                <asp:Label ID="lblTempNight" runat="server"/>
+            
+            </td>
+        </tr>
+               <%--<tr>
+            <td>
+                Times: 
+                Sunrise:
+                <asp:Label ID="lblTimeSunrise" runat="server"/>
+                Sunset:
+                <asp:Label ID="lblTimeSunset" runat="server"/>
+                
+            
+            </td>
+        </tr>--%>
+
+        </table>
+        
+
+
+    </form>
+</body>
+</html>
---
+++ OpenWeather/Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace OpenWeather
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GetWeatherInfo(object sender, EventArgs e)
+        {
+            string appid = "de6d52c2ebb7b1398526329875a49c57";
+            string url = string.Format("http://api.openweathermap.org/data/2.5/forecast/daily?q={0}&units=metric&cnt=1&APPID={1}", txtCity.Text, appid);
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                WeatherInfo weatherInfo = (new JavaScriptSerializer()).Deserialize<WeatherInfo>(json);
+                lblCity_Country.Text = weatherInfo.city.name + ", " + weatherInfo.city.country;
+                lblDescription.Text = weatherInfo.list[0].weather[0].description;
+                lblTempMin.Text = string.Format("{0}.c", Math.Round(weatherInfo.list[0].temp.min, 1));
+                lblTempMax.Text = string.Format("{0}.c", Math.Round(weatherInfo.list[0].temp.max, 1));
+                lblTempDay.Text = string.Format("{0}.c", Math.Round(weatherInfo.list[0].temp.day, 1));
+                lblTempNight.Text = string.Format("{0}.c", Math.Round(weatherInfo.list[0].temp.night, 1));
+                lblHumidity.Text = weatherInfo.list[0].humidity.ToString();
+                //lblTimeSunrise.Text = ;
+                tblWeather.Visible = true;
+
+            }
+        }
+
+        public class WeatherInfo
+        {
+            public City city { get; set; }
+            public List<List> list { get; set; }
+        }
+
+        public class City
+        {
+            public string name { get; set; }
+            public string country { get; set; }
+
+        }
+
+        public class Temp
+        {
+            public double day { get; set; }
+            public double min { get; set; }
+            public double max { get; set; }
+            public double night { get; set; }
+        }
+        public class Weather
+        {
+            public string description { get; set; }
+
+        }
+        public class List
+        {
+            public Temp temp { get; set; }
+            public int humidity { get; set; }
+            public List<Weather> weather { get; set; }
+        }
+        public class Sys
+        {
+            public string country { get; set; }
+            public int sunrise { get; set; }
+            public int sunset { get; set; }
+        }
+    }
+}
